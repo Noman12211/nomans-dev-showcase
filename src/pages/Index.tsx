b88300637afb0a9e934a6dc9eb00252a02e69197
atif <atif@itbrainsolutions.co.uk>
2025-09-26 15:48:54 +0500

    New git

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -8,7 +8,7 @@ import Education from "@/components/Education";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
-const Index = () => {
+const Index = () => { 
   return (
     <div className="min-h-screen bg-background">
       <Navigation />
